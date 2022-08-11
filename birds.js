b3e68b8eac7206c5db798a1e71eb7bb28d6faab6
Austin <ariggs7@gmail.com>
2022-08-10 19:46:41 -0500

    added http functions and birds.js

---
+++ birds.js
@@ -0,0 +1,7 @@
+let birds = [
+  {id: '1', name: 'Bird 1'},
+  {id: '2', name: 'Bird 2'},
+  {id: '3', name: 'Bird 3'}
+]
+
+module.exports = birds
---
+++ index.js
@@ -0,0 +1,5 @@
+array = [1,2,3,4];
+
+if (1 in array) {
+  console.log('yay');
+}
